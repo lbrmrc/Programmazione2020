91b7b00c3bcf324884ffadb3ba8e3ab7b8915f31
Marco Alberti <malbertife@gmail.com>
2020-11-02 17:33:00 +0100

    esempio concatenazione

--- codice/095_stringhe/concatenazione.c
+++ codice/095_stringhe/concatenazione.c
@@ -5,21 +5,20 @@ int main() {
   char b[] = "luca";
   char c[20];
   int i, j;
-  // copio a in c;
+  // copio a in c
   i = 0;
   while (a[i] != '\0') {
     c[i] = a[i];
     i++;
   }
+
   // copio b in c
   j = 0;
   while (b[j] != '\0') {
     c[i + j] = b[j];
     j++;
   }
-  // imposto il terminatore all'indice corrispondente alla somma delle lunghezze
-  // di a e b
-  c[i + j] = '\0';
 
-  return 0;
+  c[i + j] = '\0';
+  printf("%s\n", c);
 }
