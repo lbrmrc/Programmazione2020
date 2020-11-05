0dadc9bd48e005ff8d4599038fe19b937975e93b
Marco Alberti <malbertife@gmail.com>
2020-11-05 09:15:31 +0100

    supporto esponente negativo

--- codice/l06/potenza.c
+++ codice/l06/potenza.c
@@ -1,6 +1,23 @@
-int potenza(int base, int esp) {
+#include <stdio.h>
+
+float potenza(int base, int esp) {
   int cont, prod = 1;
-  for (cont = 0; cont < esp; cont++)
-    prod *= base;
-  return prod;
+  if (esp < 0) {
+    esp = -1 * esp;
+    for (cont = 0; cont < esp; cont++)
+      prod *= base;
+    return 1 / (float)prod;
+  } else {
+    for (cont = 0; cont < esp; cont++)
+      prod *= base;
+    return prod;
+  }
+}
+
+int main() {
+  printf("%f\n", potenza(2, 3));   // atteso: 8
+  printf("%f\n", potenza(2, 0));   // atteso: 1
+  printf("%f\n", potenza(2, -3));  // atteso: 0.125
+
+  return 0;
 }
