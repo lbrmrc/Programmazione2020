ae5545bc136664db782a58c6b620e77a6e5cf622
Marco Alberti <malbertife@gmail.com>
2020-11-05 10:40:44 +0100

    PI (con funzioni)

--- codice/l06/PI.c
+++ codice/l06/PI.c
@@ -4,20 +4,20 @@ int primo(int M) {
   // primo(5) -> 1
   // primo(8) -> 0
   int j, cont = 0;
-  for (j = 1; j <= M; j++) {
+  for (j = 2; j * j <= M; j++) {
     if (M % j == 0)
-      cont++;
+      // j è un divisore di M compreso fra 2 e M-1
+      return 0;
   }
-  if (cont == 2)
-    return 1;
-  else
-    return 0;
+  return 1;
 }
 
 int PI(int n) {
   int i, primi = 0;
   for (i = 2; i <= n; i++) {
+    // i assume i valori della la sequenza 2..n-1
     if (primo(i))
+      // i assume i valori della sequenza di numeri primi compresi fra 2 e n- 1
       primi++;
   }
   return primi;
