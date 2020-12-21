2c30d5b58b526d8a77741cf012b5c53195b8775b
Marco Alberti <malbertife@gmail.com>
2020-12-21 17:46:36 +0100

    controllo valore ritorno malloc

--- codice/230_liste_collegate/insTesta.c
+++ codice/230_liste_collegate/insTesta.c
@@ -1,8 +1,9 @@
-#include <malloc.h>
+#include <stdlib.h>
 #include "tipi.h"
 
 void insTesta(Lista* pl, Dato d) {
   Nodo* aux = (Nodo*)malloc(sizeof(Nodo));
+  if (aux == NULL) exit(100); // allocazione non riuscita
   aux->dato = d;
   aux->next = *pl;
   *pl = aux;
