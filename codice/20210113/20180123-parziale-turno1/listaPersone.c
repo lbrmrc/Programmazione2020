0cbc73dd6968f96ddca3d41a20e32a6cc1a43bce
Marco Alberti <malbertife@gmail.com>
2021-01-13 11:46:30 +0100

    Esercizio 1

---
+++ codice/20210113/20180123-parziale-turno1/listaPersone.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "listaPersone.h"
+
+void nuovaLista(Lista* pl) {
+  *pl = NULL;
+}
+
+void insTesta(Lista* pl, Persona d) {
+  Nodo* aux = (Nodo*)malloc(sizeof(Nodo));
+  if (aux == NULL) {
+    printf("Errore allocazione memoria\n");
+    exit(3);
+  }
+  aux->dato = d;
+  aux->next = *pl;
+  *pl = aux;
+}
+
+Lista* ricerca(Lista* pl, int anno) {
+  // salto gli elementi che devono precedere quello da inserire
+  while (*pl != NULL && (*pl)->dato.anno_nascita < anno)
+    pl = &(*pl)->next;
+  return pl;
+}
+
+void insOrd(Lista* pl, Persona d) {
+  // inserisco d in testa alla prima lista
+  // che è vuota o ha testa che segue d
+
+  // trovo la lista in testa alla quale inserire l'elemento
+  pl = ricerca(pl, d.anno_nascita);
+  // inserisco l'elemento
+  insTesta(pl, d);
+}
+
+void stampa(Lista l) {
+  // ForEach (stampaElemento, l)
+  while (l != NULL) {
+    printf("%s %d\n", l->dato.nome, l->dato.anno_nascita);
+    l = l->next;
+  }
+}
---
+++ codice/20210113/20180123-parziale-turno1/listaPersone.h
@@ -0,0 +1,15 @@
+typedef struct {
+    char nome[30];
+    int anno_nascita;
+} Persona;
+
+typedef struct nodo {
+    Persona dato;
+    struct nodo *next;
+} Nodo;
+
+typedef Nodo *Lista;
+
+void nuovaLista(Lista *pl);
+void insOrd(Lista *pl, Persona d);
+void stampa(Lista l);
